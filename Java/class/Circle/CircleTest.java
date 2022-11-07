0a386a05692f1089059dc608ae4c00d17dd034cc
Krestfell <krestfell@gmail.com>
2022-11-06 23:24:11 -0600

    h

---
+++ Java/class/Circle/CircleTest.java
@@ -0,0 +1,8 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+
+        System.out.println("The radius is " + c1.getRadius());
+        System.out.println("The area is " + c1.getArea());
+    }
+}
---
+++ Java/class/demo/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+
+    @Override
+    public String toString() {
+        return "Main []";
+    }
+
+}
